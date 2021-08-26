8694ade8e51f28047ab1bfc57e960cbf377c3659
Daniel Erenrich <daniel@erenrich.net>
2021-08-26 17:51:20 -0400

    add script to bundle the source

---
+++ bundle_source.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# needed for submitting to the firefox extension store
+
+cd ..
+zip -j -r wwwyzzerdd/dist/wwwyzzerdd_source.zip wwwyzzerdd/*json wwwyzzerdd/*sh wwwyzzerdd/src/*ts wwwyzzerdd/src/*json wwwyzzerdd/*md wwwyzzerdd/LICENSE
